e21910c1b2ad358a7bbcd589501bf7c4abc93a7a
Sir-pluto <oyediranjoshua0004@gmail.com>
2024-05-21 14:31:13 +0100

    Adding some models like Tools and Skills

--- app/Models/Tools.php
+++ app/Models/Tools.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Skills extends Model
+class Tools extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'Skills'
-
+        'Tools'
     ];
 }
